4135e533eb884ed2a2d8a62ef458a37d4c6e941f
cryptoJLee <chris.jin84@gmail.com>
2023-01-09 17:25:22 -0500

    adds time

--- test/Appointment.test.js
+++ test/Appointment.test.js
@@ -34,6 +34,17 @@ describe("Appointment", () => {
 
 describe("AppointmentsDayView", () => {
   let container;
+  const today = new Date();
+  const twoAppointments = [
+    {
+      startsAt: today.setHours(12, 0),
+      customer: { firstName: "Ashley" },
+    },
+    {
+      startsAt: today.setHours(13, 0),
+      customer: { firstName: "Jordan" },
+    },
+  ]
 
   beforeEach(() => {
     container = document.createElement("div");
@@ -59,14 +70,29 @@ describe("AppointmentsDayView", () => {
   })
 
   it("renders an li for each appointment", () => {
-    const today = new Date();
-    const twoAppointments = [
-      { startsAt: today.setHours(12, 0) },
-      { startsAt: today.setHours(13, 0) },
-    ]
+
     render(<AppointmentsDayView appointments={twoAppointments} />);
     const listChildren = document.querySelectorAll("ol > li");
-    console.log(listChildren)
     expect(listChildren).toHaveLength(2);
   })
+
+  it("renders the time of each appointment", () => {
+
+    render(<AppointmentsDayView appointments={twoAppointments} />);
+    const listChildren = document.querySelectorAll("ol > li");
+    expect(listChildren[0].textContent).toEqual("12:00");
+    expect(listChildren[1].textContent).toEqual("13:00");
+  });
+
+  it("initially shows a message saying there are no appointments today", () => {
+    render(<AppointmentsDayView appointments={[]} />);
+    expect(document.body.textContent).toContain("There are no appointments scheduled for today");
+  });
+
+  it("selects the first appointment by default", () => {
+    render(
+      <AppointmentsDayView appointments={twoAppointments} />
+    );
+    expect(document.body.textContent).toContain("Ashley");
+  });
 });
