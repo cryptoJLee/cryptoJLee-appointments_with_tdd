e70f1fac778b025aa16a908807dbfb6d715b8704
cryptoJLee <chris.jin84@outlook.com>
2023-01-11 01:59:55 -0600

    add appointmentform

---
+++ src/AppointmentForm.js
@@ -0,0 +1,180 @@
+import React, { useCallback, useState } from "react";
+const timeIncrements = (
+  numTimes,
+  startTime,
+  increment
+) =>
+  Array(numTimes)
+    .fill([startTime])
+    .reduce((acc, _, i) =>
+      acc.concat([startTime + i * increment])
+    );
+const dailyTimeSlots = (
+  salonOpensAt,
+  salonClosesAt
+) => {
+  const totalSlots =
+    (salonClosesAt - salonOpensAt) * 2;
+  const startTime = new Date()
+    .setHours(salonOpensAt, 0, 0, 0);
+  const increment = 30 * 60 * 1000;
+  return timeIncrements(
+    totalSlots,
+    startTime,
+    increment
+  );
+};
+const toTimeValue = timestamp =>
+  new Date(timestamp).toTimeString().substring(0, 5);
+const weeklyDateValues = (startDate) => {
+  const midnight = startDate.setHours(0, 0, 0, 0);
+  const increment = 24 * 60 * 60 * 1000;
+  return timeIncrements(7, midnight, increment);
+};
+const toShortDate = (timestamp) => {
+  const [day, , dayOfMonth] = new Date(timestamp)
+    .toDateString()
+    .split(" ");
+  return `${day} ${dayOfMonth}`;
+}
+const mergeDateAndTime = (date, timeSlot) => {
+  const time = new Date(timeSlot);
+  return new Date(date).setHours(
+    time.getHours(),
+    time.getMinutes(),
+    time.getSeconds(),
+    time.getMilliseconds()
+  );
+};
+
+const RadioButtonIfAvailable = ({
+  availableTimeSlots,
+  date,
+  timeSlot,
+  checkedTimeSlot,
+  handleChange
+}) => {
+  const startsAt = mergeDateAndTime(date, timeSlot);
+
+  if (
+    availableTimeSlots.some(
+      (timeSlot) => timeSlot.startsAt === startsAt
+    )
+  ) {
+    const isChecked = startsAt === checkedTimeSlot;
+    return (
+      <input
+        name="startsAt"
+        type="radio"
+        value={startsAt}
+        checked={isChecked}
+        onChange={handleChange}
+      />
+    );
+  }
+  return null;
+};
+
+const TimeSlotTable = ({
+  salonOpensAt,
+  salonClosesAt,
+  today,
+  availableTimeSlots,
+  checkedTimeSlot,
+  handleChange,
+}) => {
+  const dates = weeklyDateValues(today);
+  const timeSlots = dailyTimeSlots(
+    salonOpensAt,
+    salonClosesAt
+  );
+  return (
+    <table id="time-slots">
+      <thead>
+        <tr>
+          <th />
+          {dates.map(d => (
+            <th key={d}>{toShortDate(d)}</th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {timeSlots.map(timeSlot => (
+          <tr key={timeSlot}>
+            <th>{toTimeValue(timeSlot)}</th>
+            {dates.map(date => (
+              <td key={date}>
+                <RadioButtonIfAvailable
+                  availableTimeSlots={availableTimeSlots}
+                  date={date}
+                  timeSlot={timeSlot}
+                  checkedTimeSlot={checkedTimeSlot}
+                  handleChange={handleChange}
+                />
+              </td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+export const AppointmentForm = ({
+  selectableServices,
+  original,
+  salonOpensAt,
+  salonClosesAt,
+  today,
+  availableTimeSlots,
+  onSubmit,
+}
+) => {
+  const [appointment, setAppointment] = useState(original);
+  const handleStartsAtChange = useCallback(
+    ({ target: { value } }) =>
+      setAppointment((appointment) => ({
+        ...appointment,
+        startsAt: parseInt(value)
+      })),
+    []
+  )
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    onSubmit(appointment);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <select
+        name="service"
+        value={original.service}
+        readOnly
+      >
+        <option />
+        {selectableServices.map(s => (
+          <option key={s}>{s}</option>
+        ))}
+      </select>
+      <TimeSlotTable
+        salonOpensAt={salonOpensAt}
+        salonClosesAt={salonClosesAt}
+        today={today}
+        availableTimeSlots={availableTimeSlots}
+        checkedTimeSlot={appointment.startsAt}
+        handleChange={handleStartsAtChange}
+      />
+      <input type="submit" value="Add" />
+    </form >
+  );
+};
+
+AppointmentForm.defaultProps = {
+  salonOpensAt: 9,
+  salonClosesAt: 19,
+  today: new Date(),
+  selectableServices: [
+    "Cut",
+    "Blow-dry",
+    "Cut & color"
+  ]
+}
--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { AppointmentForm } from "./AppointmentForm";
 import { AppointmentsDayView } from "./AppointmentsDayView";
 import { CustomerForm } from "./CustomerForm";
 import { sampleAppointments } from "./sampleData";
+
+const today = new Date();
 ReactDOM.createRoot(
   document.getElementById("root")
 ).render(
-  <CustomerForm original={{
-    firstName: "Ashley",
-    lastName: "Jamie",
-    phoneNumber: "123456",
-  }} />
+  // <CustomerForm original={{
+  //   firstName: "Ashley",
+  //   lastName: "Jamie",
+  //   phoneNumber: "123456",
+  // }} />
+  <AppointmentForm
+    original={{
+      service: "",
+      startsAt: today.setHours(9, 30, 0, 0)
+    }}
+    availableTimeSlots={[
+      { startsAt: today.setHours(9, 0, 0, 0) },
+      { startsAt: today.setHours(9, 30, 0, 0) },
+    ]}
+    today={today}
+  />
 );
